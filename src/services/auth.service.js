528bb34dcf703ae5f7f6990e8ba89565bcf522de
tien.nguyen <nguyenmtien465@gmail.com>
2024-06-04 11:33:04 +0700

    fix env

--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -1,5 +1,5 @@
 const { sign } = require('jsonwebtoken');
-const { JWT_EXPIRES_IN, JWT_SECRET } = require('../config');
+const { Env } = require('../config');
 const { HttpException } = require('../exceptions/HttpException');
 const ms = require('ms');
 const { ErrorMessage } = require('../constants/ErrorMessage');
@@ -11,17 +11,17 @@ const createToken = user => {
     username: user.username,
     roleId: user.roleId,
   };
-  const expiresIn = Date.now() + ms(JWT_EXPIRES_IN);
+  const expiresIn = Date.now() + ms(Env.JWT_EXPIRES_IN);
 
-  const token = sign(dataStoredInToken, JWT_SECRET, { expiresIn: JWT_EXPIRES_IN });
+  const token = sign(dataStoredInToken, Env.JWT_SECRET, { expiresIn: Env.JWT_EXPIRES_IN });
   return { expiresIn, token: token };
 };
 
 class AuthService {
   async login(userData) {
     const findUser = await new UserRepository().findOne({ where: { username: userData.username } });
-
     if (!findUser) throw new HttpException(401, ErrorMessage.INCORRECT_LOGIN);
+
     const isPasswordMatching = userData.password === findUser.password;
     if (!isPasswordMatching) throw new HttpException(401, ErrorMessage.INCORRECT_LOGIN);
 
